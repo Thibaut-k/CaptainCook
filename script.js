32030f47415d7e1da7aaa296b852c4d926548ead
Moise <nsingi-moise@hotmail.com>
2020-10-07 13:57:38 +0200

    Mise en place du système de redirection des liens

--- script.js
+++ script.js
@@ -28,3 +28,46 @@ const navSlide = () => {
 
 }
 navSlide();
+
+
+
+/*-------------------------------------------------
+                    REDIRECT CLASS 
+--------------------------------------------------*/
+
+class Redirect {
+
+    constructor(hrefId, pageToRedirect) {
+        this.hrefId = hrefId
+        this.pageToRedirect = pageToRedirect
+        
+        this.redirect(this.hrefId, this.pageToRedirect)
+    
+    }
+
+    /* function to redirect to another page onClick 
+        id = id name of the clicable element
+        pageName = page name, ex : index.html
+    */
+    redirect = (id, pageName) => {
+        id = document.getElementById(id)
+        id.addEventListener("click", (e) => {
+            e.preventDefault()
+            window.location.href = `${pageName}.html`
+        })
+    }
+
+}
+
+let onReady = () => {
+    new Redirect("recipes", "all-recipes")
+    new Redirect("acceuil", "index")
+}
+
+/* if page is loaded */
+if (document.readyState !== "loading") {
+    onReady()
+}
+
+/* execute the function once DOM is fully loaded */
+document.addEventListener("DOMContentLoaded", onReady);
