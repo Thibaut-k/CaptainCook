bdb82e5e0e095f6b3609d8082c4677ba0f2f08c2
Fabala Dembele <dembele.fabala@gmail.com>
2020-10-07 13:28:07 +0200

    js de la page recette

--- script.js
+++ script.js
@@ -33,7 +33,12 @@ navSlide();
 
 
 
+
+
+
+
 /************** RECETTE PAGE **************/
+
 
 
 const recetteData = [
@@ -42,36 +47,137 @@ const recetteData = [
         nbPart:"1 personnes",
         ingrediants:"zenfnzejfoznefjznfnzekf",
         lvlRecette: "Facile",
-        tempsPrepa: "30 min",
+        tempsPrepa: "3 min",
         prepa:"sdvnsdnvjk,skcnk,qsocqksncqsncksq",
+        img:""
     },
     {
         name: "Pizza à l'annanas",
-        nbPart:"1 personnes",
+        nbPart:"8 personnes",
         ingrediants:"zenfnzejfoznefjznfnzekf",
-        lvlRecette: "Facile",
-        tempsPrepa: "30 min",
-        prepa:"sdvnsdnvjk,skcnk,qsocqksncqsncksq",
+        lvlRecette: "Difficile",
+        tempsPrepa: "80 min",
+        prepa:"sdvnsdnvjk,sksocqksncqsncksq",
+        img:""
     },
     {
         name: "Ramen",
         nbPart:"1 personnes",
-        ingrediants:"zenfnzejfoznefjznfnzekf",
-        lvlRecette: "Facile",
-        tempsPrepa: "30 min",
-        prepa:"sdvnsdnvjk,skcnk,qsocqksncqsncksq",
+        ingrediants:"zenfnzejfoznedxadsazfazefafjznfnzekf",
+        lvlRecette: "Moyen",
+        tempsPrepa: "36 min",
+        prepa:"sdvnsdnvjk,qsocqksncqsncksq",
+        img:""
     },
     {
         name: "Bolognaise",
-        nbPart:"1 personnes",
+        nbPart:"5 personnes",
+        ingrediants:"zenfdqfnzejfoznefjznfnzekf",
+        lvlRecette: "Très facile",
+        tempsPrepa: "30 min",
+        prepa:"sdvnsdnvjk,skcnk,",
+        img:""
+    },
+    {
+        name: "Spaguetti",
+        nbPart:"2 personnes",
         ingrediants:"zenfnzejfoznefjznfnzekf",
         lvlRecette: "Facile",
-        tempsPrepa: "30 min",
+        tempsPrepa: "10 min",
         prepa:"sdvnsdnvjk,skcnk,qsocqksncqsncksq",
-    },
+        img:""
+    }
 
-];
+  ];
 
+
+  const nameR = document.querySelector('#dataName');
+  const nbPartR = document.querySelector('#dateNbPart');
+  const igrediR = document.querySelector('#dataIngrediants');
+  const lvlR = document.querySelector('#dataLvlRecette');
+  const tPrepaR = document.querySelector('#dataTempsPrepa');
+  const prepaR = document.querySelector('#dataPrepa');
+  const linkRecette1 = document.querySelector("#recette1");
+  const linkRecette2 = document.querySelector("#recette2");
+  const linkRecette3 = document.querySelector("#recette3");
+  const linkRecette4 = document.querySelector("#recette4");
+
+  nameR.innerHTML = recetteData[0].name;
+  nbPartR.innerHTML = recetteData[0].nbPart;
+  igrediR.innerHTML = recetteData[0].ingrediants;
+  prepaR.innerHTML = recetteData[0].prepa;
+
+
+  linkRecette1.addEventListener('click', () =>{
+    nameR.innerHTML = recetteData[1].name;
+    nbPartR.innerHTML = recetteData[1].nbPart;
+    igrediR.innerHTML = recetteData[1].ingrediants;
+    prepaR.innerHTML = recetteData[1].prepa;
+  })
+
+  linkRecette2.addEventListener('click', () =>{
+    nameR.innerHTML = recetteData[2].name;
+    nbPartR.innerHTML = recetteData[2].nbPart;
+    igrediR.innerHTML = recetteData[2].ingrediants;
+    prepaR.innerHTML = recetteData[2].prepa;
+  })
+    linkRecette3.addEventListener('click', () =>{
+        nameR.innerHTML = recetteData[3].name;
+        nbPartR.innerHTML = recetteData[3].nbPart;
+        igrediR.innerHTML = recetteData[3].ingrediants;
+        prepaR.innerHTML = recetteData[3].prepa;
+  })
+    linkRecette4.addEventListener('click', () =>{
+        nameR.innerHTML = recetteData[4].name;
+        nbPartR.innerHTML = recetteData[4].nbPart;
+        igrediR.innerHTML = recetteData[4].ingrediants;
+        prepaR.innerHTML = recetteData[4].prepa;
+  })
 
 
 /************** RECETTE PAGE **************/
+
+
+
+
+class Redirect {
+
+    constructor(hrefId, pageToRedirect) {
+        this.hrefId = hrefId
+        this.pageToRedirect = pageToRedirect
+
+        this.redirect(this.hrefId, this.pageToRedirect)
+
+    }
+
+        id = id 
+        pageName = recettePage.html
+    
+    redirect = (id, recettePage) => {
+        id = document.getElementById(id)
+        id.addEventListener("click", (e) => {
+            e.preventDefault()
+            window.location.href = $recettePage.html
+        })
+    }
+
+}
+
+let onReady = () => {
+    new Redirect("recipes", "all-recipes")
+    new Redirect("acceuil", "index")
+    new hoverEffect({
+        parent: document.getElementsByClassName("image-switch"),
+        intensity: 0.2,
+        image1: document.querySelector(".image-switch").src = "../img/home/3-250x250.jpg",
+        displacementImage: document.querySelector(".image-switch").src = "../img/home/diss.png "
+    })
+}
+
+
+if (document.readyState !== "loading") {
+    onReady()
+}
+
+
+document.addEventListener("DOMContentLoaded", onReady);
